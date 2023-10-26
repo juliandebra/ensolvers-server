d9ccb22ad413d6a1e62bc7582323087260a8299f
juliandebra <julian.debra25@gmail.com>
2023-10-26 14:14:20 -0300

    Last commit

--- server.js
+++ server.js
@@ -1,25 +1,17 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import path from 'path'
-import { fileURLToPath } from 'url';
-import { config } from 'dotenv'
-import sql from 'mssql'
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+require('dotenv').config();
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const path = require('path')
+const sql = require('mssql');
+const { default: axios } = require('axios');
+const fs = require('fs')
 const app = express();
 const port = 5000;
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-app.use(cors())
-app.use(express.static(path.join(__dirname + "/public")))
-
-config()
-
-
-let niptforms = []
-let cancerforms = []
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(cors());
+app.use(express.static(path.join(__dirname, '/public')));
 
 const connectionData = {
   server: '192.168.122.2',
@@ -38,43 +30,11 @@ const connectionData = {
   }
 };
 
-app.get('/testform', async (req, res) => {
-  try {
-    // Connect to the database
-    await sql.connect(connectionData);
-    // Query string
-    const queryString = 'SELECT * FROM nipt_forms'; // Replace with your actual table name
-    // Execute the query
-    const result = await sql.query(queryString);
-    // Print the result
-    console.log(result.recordset);
-    res.send(result.recordset)
-  } catch (error) {
-    // Handle errors
-    console.error('Error executing query:', error);
-  } finally {
-    // Close the connection
-    await sql.close();
-  }
-})
-
-
-// const connectionData = {
-//   user: 'jdebrabandere',
-//   host: '192.168.122.2',
-//   database: 'CibicForms',
-//   password: 'D3br4b4nd3rE*',
-//   port: 1433,
-// }
-
-// app.get('/testform', async (req, res) => {
-//   const result = await client.query('SELECT * FROM nipt_forms')
-//   res.send(result.rows);
-// })
-
 
 const configurateDate = (date) => {
-  if(date){
+  if (date == 'NA') {
+    return null
+  }else if(date){
     const dateComponents = date.split("/"); 
     const day = parseInt(dateComponents[0], 10);
     const month = parseInt(dateComponents[1], 10) - 1; 
@@ -100,23 +60,59 @@ const configurateDateTime = (date, time) => {
   return new Date(year, month, day, hours, minutes)
 }
 
-app.get('/niptforms', (req, res) => {
-    res.send(niptforms);
+
+app.post('/sendfile', async (req, res) => {
+  try {
+      // const response = await axios.post('http://192.168.122.5:8093/testfile', req.body);
+      const response = await axios.post('http://192.168.122.18:8990/sendfile', req.body);
+      console.log('Response from server:', response.data);
+      res.status(200).json(response.data);
+  } catch (error) {
+      console.error('Error connecting to server:', error);
+      res.status(500).json({ error: 'Server connection error' });
+  }
+});
+
+
+
+app.get('/niptforms', async (req, res) => {
+  try {
+    await sql.connect(connectionData);
+    const queryString = 'SELECT * FROM nipt_forms';
+    const result = await sql.query(queryString);
+    console.log(result.recordset);
+    res.send(result.recordset)
+  } catch (error) {
+    console.error('Error executing query:', error);
+  } finally {
+    await sql.close();
+  }
 })
-app.get('/cancerforms', (req, res) => {
-  res.send(cancerforms);
+app.get('/cancerforms', async (req, res) => {
+  try {
+    await sql.connect(connectionData);
+    const queryString = 'SELECT * FROM cancer_forms';
+    const result = await sql.query(queryString);
+    console.log(result.recordset);
+    res.send(result.recordset)
+  } catch (error) {
+    console.error('Error executing query:', error);
+  } finally {
+    await sql.close();
+  }
 })
 
 app.post('/niptforms', async (req, res) => {
   try {
     await sql.connect(connectionData);
-    const {formType, fetalSex, patient, pregnancyInfo, studyInfo, sampleInfo, background, aplication, entry, termsAndCondition, dateAndHour} = req.body
+    const {formType, fetalSex, patient, pregnancyInfo, studyInfo, sampleInfo, background, aplication, entry, termsAndCondition, dateAndHour, user, os, externID} = req.body
     const parsedDateAndHour = dateAndHour.split(' ')
     const dateAndHourModified = configurateDateTime(parsedDateAndHour[0], parsedDateAndHour[1])
     const sampleDateModified = configurateDate(sampleInfo.sampleDate)
     const pregnancyTerminationModified = configurateDate(sampleInfo.pregnancyTermination)
-
-    const text = 'INSERT INTO nipt_forms ("tipo_de_formulario", "desea_saber_el_sexo_fetal", "peso", "altura", "embarazo_gemelar", "si_es_mono_bicorial_no_sabe", "gemelo_reabsorbido", "tratamiento_de_fertilidad", "si_es_si_cual", "si_corresponde_edad_ovodotante", "si_corresp_edad_criopreserv", "edad_maternal_avanzada", "screening_trimestre_alterado", "anomalias_ecograficas_fetales", "voluntad_maternal", "observaciones", "semanas_gestacion", "dias_gestacion", "abortos_previos", "motivo_repeticion", "subrrogacion_de_utero", "nombre_padre_progenitor", "nombre_madre_progenitora", "antecedentes_de_cancer", "recibio_transplante", "sexo_donante", "nro_solicitud", "nro_ingreso", "id_anterior", "archivo_tipo_de_estudio", "terminos_y_condiciones", "fecha_de_envio", "fecha_toma_muestra", "fecha_aprox_det_gest") VALUES (@Value1, @Value2, @Value3, @Value4, @Value5, @Value6, @Value7, @Value8, @Value9, @Value10, @Value11, @Value12, @Value13, @Value14, @Value15, @Value16, @Value17, @Value18, @Value19, @Value20, @Value21, @Value22, @Value23, @Value24, @Value25, @Value26, @Value27, @Value28, @Value29, @Value30, @Value31, @Value32, @Value33, @Value34)'
+    console.log(pregnancyTerminationModified)
+    console.log(sampleInfo.pregnancyTermination)
+    const text = 'INSERT INTO nipt_forms ("tipo_de_formulario", "desea_saber_el_sexo_fetal", "peso", "altura", "embarazo_gemelar", "si_es_mono_bicorial_no_sabe", "gemelo_reabsorbido", "tratamiento_de_fertilidad", "si_es_si_cual", "si_corresponde_edad_ovodotante", "si_corresp_edad_criopreserv", "edad_maternal_avanzada", "screening_trimestre_alterado", "anomalias_ecograficas_fetales", "voluntad_maternal", "observaciones", "semanas_gestacion", "dias_gestacion", "abortos_previos", "motivo_repeticion", "subrrogacion_de_utero", "nombre_padre_progenitor", "nombre_madre_progenitora", "antecedentes_de_cancer", "recibio_transplante", "sexo_donante", "nro_solicitud", "nro_ingreso", "id_anterior", "terminos_y_condiciones", "fecha_de_envio", "fecha_toma_muestra", "fecha_aprox_det_gest", "usuario", "obra_social", "id_externo") VALUES (@Value1, @Value2, @Value3, @Value4, @Value5, @Value6, @Value7, @Value8, @Value9, @Value10, @Value11, @Value12, @Value13, @Value14, @Value15, @Value16, @Value17, @Value18, @Value19, @Value20, @Value21, @Value22, @Value23, @Value24, @Value25, @Value26, @Value27, @Value28, @Value29, @Value30, @Value31, @Value32, @Value33, @Value34, @Value35, @Value36)'
     const values = [
       formType.toString(), 
       fetalSex, 
@@ -147,11 +143,13 @@ app.post('/niptforms', async (req, res) => {
       aplication,
       entry,
       sampleInfo.prevId,
-      studyInfo.file,
       termsAndCondition,
       dateAndHourModified,
       sampleDateModified,
-      pregnancyTerminationModified
+      pregnancyTerminationModified,
+      user,
+      os,
+      externID
     ]
 
     const request = new sql.Request();
@@ -172,11 +170,12 @@ app.post('/niptforms', async (req, res) => {
 app.post('/cancerforms', async (req, res) => {
   try{
     await sql.connect(connectionData);
-    const {formType, aplication, entry, studyInfo, patientEthnic, patientInfo, termsAndCondition, postTest, authorizedInfo, dateAndHour} = req.body
+    const {formType, aplication, entry, user, os, studyInfo, patientEthnic, patientInfo, termsAndCondition, postTest, authorizedInfo, dateAndHour} = req.body
     
     const parsedDateAndHour = dateAndHour.split(' ')
     const dateAndHourModified = configurateDateTime(parsedDateAndHour[0], parsedDateAndHour[1])
-    const text = 'INSERT INTO cancer_forms ("tipo_de_formulario", "nro_solicitud", "nro_ingreso", "panel", "nueva_muestra", "id_anterior", "etnia", "antecedentes", "edad_al_diagnostico", "tratamientos_realizados", "tipo_de_cancer", "subtipo_de_cancer_de_mama", "subtipo_de_cancer_de_ovario", "polipos", "resultado_msi", "metilacion", "valor_de_metilacion", "otro_tipo_de_cancer", "si_es_si_cual", "antecedentes_familiares", "de_que_tipo_y_de_quien", "estudios_moleculares", "orden_medica", "genograma", "motivo_de_reapertura", "observaciones", "terminos_y_condiciones", "post_test", "informacion_autorizada", "fecha_de_envio") VALUES (@Value1, @Value2, @Value3, @Value4, @Value5, @Value6, @Value7, @Value8, @Value9, @Value10, @Value11, @Value12, @Value13, @Value14, @Value15, @Value16, @Value17, @Value18, @Value19, @Value20, @Value21, @Value22, @Value23, @Value24, @Value25, @Value26, @Value27, @Value28, @Value29, @Value30)'
+    const concatCancerType = patientInfo.cancerType == 'NA' ? 'NA' : patientInfo.cancerType.length === 1 ? patientInfo.cancerType[0] : patientInfo.cancerType.join(', ')
+    const text = 'INSERT INTO cancer_forms ("tipo_de_formulario", "nro_solicitud", "nro_ingreso", "panel", "nueva_muestra", "id_anterior", "etnia", "antecedentes", "edad_al_diagnostico", "tratamientos_realizados", "tipo_de_cancer", "subtipo_de_cancer_de_mama", "subtipo_de_cancer_de_ovario", "polipos", "resultado_msi", "metilacion", "valor_de_metilacion", "otro_tipo_de_cancer", "si_es_si_cual", "antecedentes_familiares", "de_que_tipo_y_de_quien", "estudios_moleculares", "orden_medica", "genograma", "motivo_de_reapertura", "observaciones", "terminos_y_condiciones", "post_test", "informacion_autorizada", "fecha_de_envio", "usuario", "obra_social") VALUES (@Value1, @Value2, @Value3, @Value4, @Value5, @Value6, @Value7, @Value8, @Value9, @Value10, @Value11, @Value12, @Value13, @Value14, @Value15, @Value16, @Value17, @Value18, @Value19, @Value20, @Value21, @Value22, @Value23, @Value24, @Value25, @Value26, @Value27, @Value28, @Value29, @Value30, @Value31, @Value32)'
     const values = [
       formType,
       aplication,
@@ -188,7 +187,7 @@ app.post('/cancerforms', async (req, res) => {
       patientInfo.background ,
       patientInfo.diagnosisAge ,
       patientInfo.treatmentDone ,
-      patientInfo.cancerType ,
+      concatCancerType ,
       patientInfo.mamalCancerSubtype ,
       patientInfo.ovaryCancerSubtype ,
       patientInfo.polyps ,
@@ -207,7 +206,9 @@ app.post('/cancerforms', async (req, res) => {
       termsAndCondition,
       postTest,
       authorizedInfo,
-      dateAndHourModified
+      dateAndHourModified,
+      user,
+      os
     ]
     const request = new sql.Request();
     for (let i = 0; i < values.length; i++) {
@@ -229,8 +230,4 @@ app.all('/*', (req, res, next) => {
 });
 
 app.listen(port);
-
-
-// console.log('pregnancyTermination',sampleInfo.pregnancyTermination)
-// console.log('sampleDate', sampleInfo.sampleDate)
-
+//En IIS, reemplazar por app.listen(process.env.PORT); 
