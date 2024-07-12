f986483f61daa7dfa656a28f20a3c035c77347cf
juliandebra <julian.debra25@gmail.com>
2024-07-12 01:08:35 -0300

    Migration and SSL

--- config/database.js
+++ config/database.js
@@ -1,17 +1,30 @@
-require('dotenv').config()
-const { Sequelize } = require('sequelize')
+require("dotenv").config();
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+const { Sequelize } = require("sequelize");
+
+const caCert = process.env.CA_PEM;
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
+  dialect: "postgres",
   define: {
     timestamps: false,
   },
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false, 
+      ca: caCert,
     },
   },
-})
+  logging: (msg) => console.log(`[Sequelize] ${msg}`),
+});
 
-module.exports = sequelize
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Conexión a la base de datos exitosa");
+  })
+  .catch((err) => {
+    console.error("Error al conectar a la base de datos:", err);
+  });
+
+module.exports = sequelize;
