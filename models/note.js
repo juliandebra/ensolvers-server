8453dd5e764f37af13b96df9b07b0ff366d467a1
juliandebra <julian.debra25@gmail.com>
2025-02-10 10:50:24 -0300

    vercel config

--- models/note.js
+++ models/note.js
@@ -1,48 +1,52 @@
-const { DataTypes } = require('sequelize')
-const sequelize = require('../config/database')
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
 
-const Note = sequelize.define('Note', {
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      notNull: {
-        msg: 'Title is required.',
-      },
-      notEmpty: {
-        msg: 'Title cannot be empty.',
-      },
-    },
-  },
-  description: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-    validate: {
-      notNull: {
-        msg: 'Description is required.',
-      },
-      notEmpty: {
-        msg: 'Description cannot be empty.',
+const Note = sequelize.define(
+  "Note",
+  {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "Title is required.",
+        },
+        notEmpty: {
+          msg: "Title cannot be empty.",
+        },
       },
     },
-  },
-  date: {
-    type: DataTypes.DATE,
-    allowNull: false,
-    validate: {
-      notNull: {
-        msg: 'Date is required',
-      },
-      isDate: {
-        msg: 'Must be a valid date',
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "Description is required.",
+        },
+        notEmpty: {
+          msg: "Description cannot be empty.",
+        },
       },
     },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "Date is required",
+        },
+        isDate: {
+          msg: "Must be a valid date",
+        },
+      },
+    },
+    tags: {
+      type: DataTypes.STRING,
+    },
   },
-  tags: {
-    type: DataTypes.STRING,
-  },
-}, {
-  tableName: 'notes',
-})
+  {
+    tableName: "notes",
+  }
+);
 
-module.exports = Note
+module.exports = Note;
--- server.js
+++ server.js
@@ -1,27 +1,28 @@
-require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const path = require('path')
-const app = express()
-const port = 5000
-app.use(bodyParser.json({ limit: '50mb' }))
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
-app.use(cors())
-app.use(express.static(path.join(__dirname, '/public')))
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+const port = 5000;
 
-const notesRouter = require('./routes/notes')
-const filtersRouter = require('./routes/filters')
-app.use('/notes', notesRouter)
-app.use('/filters', filtersRouter)
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(cors());
+app.use(express.static(path.join(__dirname, "/public")));
 
-process.on('SIGINT', async () => {
-  await client.end()
-  process.exit()
-})
+const notesRouter = require("./routes/notes");
+const filtersRouter = require("./routes/filters");
+app.use("/notes", notesRouter);
+app.use("/filters", filtersRouter);
 
-app.all('/*', (req, res, next) => {
-    res.send('That route does not exist!')
-})
+process.on("SIGINT", async () => {
+  await client.end();
+  process.exit();
+});
 
-app.listen(port)
+app.all("/*", (req, res, next) => {
+  res.send("That route does not exist!");
+});
+
+app.listen(port);
